3a23828ad87e49b9645ce9d4a811b4f1396576af
rkddnr330 <rkddnr330@nate.com>
2021-05-27 19:00:56 +0900

    배포하자

--- CRUD/main/urls.py
+++ CRUD/main/urls.py
@@ -5,9 +5,7 @@ from . import views
 app_name = "main"
 
 urlpatterns=[
-
-    
-    path('/create',views.create,name="create"),
+    path('create',views.create,name="create"),
     path('detail/<int:write_id>',views.detail,name="detail"),
     path('update/<int:write_id>',views.update,name="update"),
     path('delete/<int:write_id>',views.delete, name="delete"),
